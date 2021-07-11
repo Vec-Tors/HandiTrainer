46f9f169c807a5df6f047238d718925b5cf344dc
builderpepc <troy.hiruka@gmail.com>
2021-07-10 20:04:26 -0400

    lineSplit() and renderChallengeText() functions

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function setDifficulty(d) {
 
 function countLetters(text) { 
     // Count the characters in a str without spaces (includes punctuation)
-    var length = 0;
+    let length = 0;
     for (i = 0; i < text.length; i++) {
         if (text[i] != ' ') {
             length++;
@@ -38,6 +38,36 @@ function genText() {
     }
 
     return text;
+}
+
+function lineSplit(text) {
+    // Split a one-line string into an array of lines
+    const LINE_WORD_COUNT = 10;
+    let lines = [];
+    words = text.split(' ');
+    for (i = 0; i < words.length; i++) {
+        if (lines.length == 0) {
+            lines.push("");
+        }
+
+        if (lines[lines.length - 1].split(' ').length < LINE_WORD_COUNT) {
+            lines[lines.length - 1] = lines[lines.length - 1] + words[i] + ' ';
+        }
+        else {
+            lines.push(words[i] + ' ');
+        }
+    }
+
+    return lines;
+}
+
+function renderChallengeText(lines) {
+    // Take an array of strings and display them in the challenge text div
+    let box = document.getElementById("challenge-text");
+    box.innerHTML = "";
+    for (i = 0; i < lines.length; i++) {
+        box.innerHTML += `<br><p class="challenge-text">${lines[i]}</p>`
+    }
 }
 
 setDifficulty(20);
